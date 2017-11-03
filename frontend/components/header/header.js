785563461061237848cbe9fbdb7aa344c992cc9c
maxim-buslaev <maksimys941@gmail.com>
2017-11-03 17:06:01 +0300

    no message

--- frontend/components/header/header.js
+++ frontend/components/header/header.js
@@ -1,13 +1,13 @@
-; (function () {
+(function() {
   headerMenu();
   menu_all_products();
 
   function headerMenu() {
     var menu = $("#showMainMenu");
 
-    menu.click(function () {
-      if ($("html").hasClass('block-html')) {
-        $("html").removeClass('block-html');
+    menu.click(function() {
+      if ($("html").hasClass("block-html")) {
+        $("html").removeClass("block-html");
       } else {
         $("html").addClass("block-html");
       }
@@ -23,54 +23,57 @@
       titleMenu = menuCatalogBody.find(".header-menu-catalog-body__title p"),
       currentLevel = 0;
 
-
-    catalogButton.click(function () {
-      if (!catalogButtonParent.hasClass('active')) {
+    catalogButton.click(function() {
+      if (!catalogButtonParent.hasClass("active")) {
         menuCatalogBody.css("max-height", "none");
-        // $("html").css('overflow-y', 'hidden');
         $("html").addClass("block-html");
-        catalogButtonParent.addClass('active');
-
-
-        // $(document).mouseup(function (e) { // событие клика по веб-документу
-        //   var div = $(".header .all-product .menu"); // тут указываем ID элемента
-        //   if (!div.is(e.target) // если клик был не по нашему блоку
-        //     && div.has(e.target).length === 0) { // и не по его дочерним элементам
-        //     $(".header .all-product .menu").css("max-height", "0");
-        //     $(".header .all-product .fon").css("display", "none");
-        //     $("html").css('overflow-y', 'auto');
-        //     $(".header .all-product").removeClass('active');
-        //   }
-        // });
+        catalogButtonParent.addClass("active");
       } else {
         menuCatalogBody.css("max-height", "0");
-        // $('html').css('overflow-y', 'auto');
-        $("html").removeClass('block-html');
-        catalogButtonParent.removeClass('active');
+        $("html").removeClass("block-html");
+        catalogButtonParent.removeClass("active");
       }
     });
 
     if (document.documentElement.clientWidth < 1024) {
-      $(".header-menu-catalog-body__submenu").click(function () {
+      $(".header-menu-catalog-body__submenu").click(function() {
         currentLevel++;
         downButton.addClass("shown");
 
         if (currentLevel == 1) {
-          $(this).parent().addClass("open");
-          let height_title = parseFloat($(this).parent().find("> a").css("height")) - 1;
-          $(this).css("top", (height_title > 50) ? height_title : 50);
+          $(this)
+            .parent()
+            .addClass("open");
+          let height_title =
+            parseFloat(
+              $(this)
+                .parent()
+                .find("> a")
+                .css("height")
+            ) - 1;
+          $(this).css("top", height_title > 50 ? height_title : 50);
         } else {
           let oldOpenLi = $(this).closest(".open");
           oldOpenLi.removeClass("open");
-          oldOpenLi.find("> .header-menu-catalog-body__submenu").css("top", "0");
-          $(this).parent().addClass("open");
-          let height_title = parseFloat($(this).parent().find("> a").css("height")) - 1;
-          $(this).css("top", (height_title > 50) ? height_title : 50);
+          oldOpenLi
+            .find("> .header-menu-catalog-body__submenu")
+            .css("top", "0");
+          $(this)
+            .parent()
+            .addClass("open");
+          let height_title =
+            parseFloat(
+              $(this)
+                .parent()
+                .find("> a")
+                .css("height")
+            ) - 1;
+          $(this).css("top", height_title > 50 ? height_title : 50);
           event.stopPropagation();
         }
       });
 
-      downButton.click(function () {
+      downButton.click(function() {
         let currentOpenedElem = menuCatalogBody.find(".open");
         currentOpenedElem.removeClass("open");
         currentLevel--;
@@ -79,22 +82,22 @@
         } else {
           let parentElem = currentOpenedElem.parent().closest("li");
           parentElem.addClass("open");
-          let height_title = parseFloat(parentElem.find("> a").css("height")) - 1;
-          parentElem.find("ul").css("top", (height_title > 50) ? height_title : 50);
+          let height_title =
+            parseFloat(parentElem.find("> a").css("height")) - 1;
+          parentElem
+            .find("ul")
+            .css("top", height_title > 50 ? height_title : 50);
           event.stopPropagation();
         }
       });
 
-      closeButton.click(function () {
-        if (catalogButtonParent.hasClass('active')) {
+      closeButton.click(function() {
+        if (catalogButtonParent.hasClass("active")) {
           menuCatalogBody.css("max-height", "0");
-          $("html").removeClass('block-html');
-          catalogButtonParent.removeClass('active');
+          $("html").removeClass("block-html");
+          catalogButtonParent.removeClass("active");
         }
       });
     }
   }
-
-
-
-}());
+})();
